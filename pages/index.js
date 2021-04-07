4516997f9c34b2126a24a02a54cfa8109ad6a0da
saurabhshiradkar <saurabhshiradkar@gmail.com>
2021-04-07 15:32:24 +0530

    Added CSS modules

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,10 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div>
-      <h1>Homepage</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro minus veritatis laborum delectus tempore. Distinctio, illum rerum. Blanditiis amet quas vero unde. Maiores dolorum commodi cum molestias, minus architecto obcaecati?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro minus veritatis laborum delectus tempore. Distinctio, illum rerum. Blanditiis amet quas vero unde. Maiores dolorum commodi cum molestias, minus architecto obcaecati?</p>
-      <Link href="/ninjas"><a>See Ninja Listing</a></Link>
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro minus veritatis laborum delectus tempore. Distinctio, illum rerum. Blanditiis amet quas vero unde. Maiores dolorum commodi cum molestias, minus architecto obcaecati?</p>
+      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro minus veritatis laborum delectus tempore. Distinctio, illum rerum. Blanditiis amet quas vero unde. Maiores dolorum commodi cum molestias, minus architecto obcaecati?</p>
+      <Link href="/ninjas"><a className={styles.btn}>See Ninja Listing</a></Link>
     </div>
   )
 }

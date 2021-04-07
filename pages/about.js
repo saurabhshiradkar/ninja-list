03d1790335d19f3ea1abf5720b4a9a33b4e170c5
saurabhshiradkar <saurabhshiradkar@gmail.com>
2021-04-07 16:13:11 +0530

    Added logo Head Component and metadata

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,17 @@
+import Head from 'next/head'
 const About = () => {
     return ( 
-        <div>
-            <h1>About</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis totam quod quas ut ratione dolores, incidunt magni inventore laudantium placeat fuga eius? Provident commodi dolore velit id dolorem corrupti nostrum?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis totam quod quas ut ratione dolores, incidunt magni inventore laudantium placeat fuga eius? Provident commodi dolore velit id dolorem corrupti nostrum?</p>
-        </div>
+        <>
+            <Head>
+            <title>Ninja List | About </title>
+            <meta name="keywords" content="ninjas"/>
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis totam quod quas ut ratione dolores, incidunt magni inventore laudantium placeat fuga eius? Provident commodi dolore velit id dolorem corrupti nostrum?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis totam quod quas ut ratione dolores, incidunt magni inventore laudantium placeat fuga eius? Provident commodi dolore velit id dolorem corrupti nostrum?</p>
+            </div>
+        </>
      );
 }
  
--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,17 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro minus veritatis laborum delectus tempore. Distinctio, illum rerum. Blanditiis amet quas vero unde. Maiores dolorum commodi cum molestias, minus architecto obcaecati?</p>
-      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro minus veritatis laborum delectus tempore. Distinctio, illum rerum. Blanditiis amet quas vero unde. Maiores dolorum commodi cum molestias, minus architecto obcaecati?</p>
-      <Link href="/ninjas"><a className={styles.btn}>See Ninja Listing</a></Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home </title>
+        <meta name="keywords" content="ninjas"/>
+      </Head>
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro minus veritatis laborum delectus tempore. Distinctio, illum rerum. Blanditiis amet quas vero unde. Maiores dolorum commodi cum molestias, minus architecto obcaecati?</p>
+        <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro minus veritatis laborum delectus tempore. Distinctio, illum rerum. Blanditiis amet quas vero unde. Maiores dolorum commodi cum molestias, minus architecto obcaecati?</p>
+        <Link href="/ninjas"><a className={styles.btn}>See Ninja Listing</a></Link>
+      </div>
+    </>
   )
 }

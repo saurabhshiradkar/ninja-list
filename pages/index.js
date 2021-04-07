1b2382d8e148768e2f109209e43b00158e6c49fa
saurabhshiradkar <saurabhshiradkar@gmail.com>
2021-04-07 12:20:22 +0530

    Created Layout Component

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,14 @@
 import Head from 'next/head'
-import Footer from '../comps/Footer'
-import Navbar from '../comps/Navbar'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div>
-      <Navbar/>
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro minus veritatis laborum delectus tempore. Distinctio, illum rerum. Blanditiis amet quas vero unde. Maiores dolorum commodi cum molestias, minus architecto obcaecati?</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro minus veritatis laborum delectus tempore. Distinctio, illum rerum. Blanditiis amet quas vero unde. Maiores dolorum commodi cum molestias, minus architecto obcaecati?</p>
-      <Footer/>
+      <Link href="/ninjas"><a>See Ninja Listing</a></Link>
     </div>
   )
 }

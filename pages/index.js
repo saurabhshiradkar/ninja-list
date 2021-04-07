cc3b6a1148094ad0dad0edf976f159b43e4ea337
saurabhshiradkar <saurabhshiradkar@gmail.com>
2021-04-07 11:57:53 +0530

    Added Navbar and Footer Components

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 import Head from 'next/head'
+import Footer from '../comps/Footer'
+import Navbar from '../comps/Navbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
+      <Navbar/>
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro minus veritatis laborum delectus tempore. Distinctio, illum rerum. Blanditiis amet quas vero unde. Maiores dolorum commodi cum molestias, minus architecto obcaecati?</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro minus veritatis laborum delectus tempore. Distinctio, illum rerum. Blanditiis amet quas vero unde. Maiores dolorum commodi cum molestias, minus architecto obcaecati?</p>
+      <Footer/>
     </div>
   )
 }

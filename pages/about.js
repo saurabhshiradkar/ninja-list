367fc372df7848fdb2476e0a8c67c1b54721b055
saurabhshiradkar <saurabhshiradkar@gmail.com>
2021-04-07 11:29:29 +0530

    Added about.js /ninjas and /ninjas/index

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return ( 
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis totam quod quas ut ratione dolores, incidunt magni inventore laudantium placeat fuga eius? Provident commodi dolore velit id dolorem corrupti nostrum?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis totam quod quas ut ratione dolores, incidunt magni inventore laudantium placeat fuga eius? Provident commodi dolore velit id dolorem corrupti nostrum?</p>
+        </div>
+     );
+}
+ 
+export default About;
